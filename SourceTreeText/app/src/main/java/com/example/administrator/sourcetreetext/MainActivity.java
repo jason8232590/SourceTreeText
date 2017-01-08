c558b1edf72bfff4bb13166154bc45f498a423f6
jason8232590 <734975410@qq.com>
2017-01-08 20:48:04 +0800

    dev版本第二次提交 1.0

--- SourceTreeText/app/src/main/java/com/example/administrator/sourcetreetext/MainActivity.java
+++ SourceTreeText/app/src/main/java/com/example/administrator/sourcetreetext/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("这是dev分支 第一次提交");
+        System.out.println("这是dev分支 1.0");
     }
 }
