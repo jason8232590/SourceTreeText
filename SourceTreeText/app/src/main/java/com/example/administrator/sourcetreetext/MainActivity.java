64f8d35de6e8409368c296c7d65dfb1800b459af
jason8232590 <734975410@qq.com>
2017-01-08 20:45:21 +0800

    Qa分支第一次提交

--- SourceTreeText/app/src/main/java/com/example/administrator/sourcetreetext/MainActivity.java
+++ SourceTreeText/app/src/main/java/com/example/administrator/sourcetreetext/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("xcqw 这是QA分支第一次提交");
     }
 }
